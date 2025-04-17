2f3893d59c42e69ae48dddeac939655e95a8eba0
vennavenkat <vennavenkat1999@gmail.com>
2025-04-16 22:11:19 -0500

    added id check

--- user-service/src/main/java/com/venkat/linkedin/user_service/service/AuthService.java
+++ user-service/src/main/java/com/venkat/linkedin/user_service/service/AuthService.java
@@ -22,6 +22,12 @@ public class AuthService {
     private  final ModelMapper modelMapper;
     private  final JWTService jwtService;
     public UserDto signUp(SignupRequestDto signupRequestDto) {
+
+        boolean exists = userRepository.existsByEmail(signupRequestDto.getEmail());
+        if (exists){
+            throw new BadRequestException("User already exists:, cannot signup again.");
+        }
+
         User user = modelMapper.map(signupRequestDto, User.class);
         user.setPassword(PasswordUtil.hashPassword(signupRequestDto.getPassword()));
 
